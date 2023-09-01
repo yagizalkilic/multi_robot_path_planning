05efc7b79fe599c128de181850e8fd2479845ad2
Yagiz Alkilic <yagiz.alkilic1@gmail.com>
2023-09-01 15:27:10 +0300

    visualization fixes

--- multi_path_scheduling/include/connected_rrt_star.h
+++ multi_path_scheduling/include/connected_rrt_star.h
@@ -1,32 +1,32 @@
-#ifndef INFORMED_RRT_STAR_H
-#define INFORMED_RRT_STAR_H
+#ifndef CONNECTED_RRT_STAR_H
+#define CONNECTED_RRT_STAR_H
 
 #include "project_utilities.h"
 
 /**
- * Implementation of an informed RRT Star.
+ * Implementation of an connected RRT Star.
  *
  * Improves upon the RRT* implementation by adding a
  * sampling strategy.
  */
-class InformedRRTStar 
+class ConnectedRRTStar 
 {
 public:
-    InformedRRTStar(int num_dimensions, std::vector<int> space_side_length, 
+    ConnectedRRTStar(int num_dimensions, std::vector<int> space_side_length, 
         std::unordered_map<std::pair<int, int>, std::unordered_map<std::pair<int, int>, bool, pair_hash>, pair_hash> dimension_pair_correspondence);
 
     void generate_tree();
-    bool is_done(const Point& point) const;
-    bool generate_node(int point_count);
-    int nearest_node(const Point& point) const;
+    bool is_done(const Point& point1, const Point& point2);
+    bool generate_node(int point_count, std::vector<Node>* some_node_list, bool is_targeted);
+    int nearest_node(const Point& point, std::vector<Node> some_node_list) const;
     bool is_valid(std::vector<Point> points);
     void find_final_path();
     void show_longest_generation() const;
     void initialize_n_space();
     std::vector<int> find_nodes_within_radius(const Node& node, double radius) const;
-    void rewire_tree(Node& new_node);
+    void rewire_tree(Node& new_node, std::vector<Node> some_node_list);
     bool is_valid_path(const Point& point1, const Point& point2);
-    std::vector<int> find_k_nearest_nodes(const Node& node, int k) const;
+    std::vector<int> find_k_nearest_nodes(const Node& node, int k, std::vector<Node> some_node_list) const;
 
     // Getter for the final path
     std::vector<Node> get_final_path() const 
@@ -37,7 +37,16 @@ public:
     // Getter for all nodes
     std::vector<Node> get_all_nodes() const 
     {
-        return node_list;
+        std::vector<Node> all_node_list;
+        for ( auto i : node_list )
+        {
+            all_node_list.push_back(i);
+        }
+        for ( auto i : target_node_list )
+        {
+            all_node_list.push_back(i);
+        }
+        return all_node_list;
     }
 
 
@@ -50,6 +59,8 @@ private:
     Point target_point;
     
     std::vector<Node> node_list;
+    std::vector<Node> target_node_list;
+
     std::vector<Node> path_nodes;
 
     int max_nodes;
@@ -64,4 +75,4 @@ private:
 
 };
 
-#endif // INFORMED_RRT_STAR_H
+#endif // CONNECTED_RRT_STAR_H
--- multi_path_scheduling/src/algorithm_tests/algorithm_test.cpp
+++ multi_path_scheduling/src/algorithm_tests/algorithm_test.cpp
@@ -3,6 +3,7 @@
 #include "../../include/rrt.h"
 #include "../../include/coordination_visualization.h"
 #include "../../include/rrt_star.h"
+#include "../../include/connected_rrt_star.h"
 #include "../../include/physical_path.h" 
 
 
@@ -18,9 +19,9 @@ int main(int argc, char *argv[])
     int AGV_radius = 10; // radius of a circular agv
     int path_min_stops = 3; // min number of times slope can be shifted
     int path_max_stops = 4; // max number of times slope can be shifted
-    int path_length_min = 50; // min length of a path segment
-    int path_length_max = 70; // max length of a path segment  
-    double segment_size = 10.0;
+    int path_length_min = 40; // min length of a path segment
+    int path_length_max = 60; // max length of a path segment  
+    double segment_size = 1.5;
 
     // Initialize the space information, determine paths and collisions on time and space
     std::cout << "Initializing coordination space..." << std::endl;
@@ -60,6 +61,16 @@ int main(int argc, char *argv[])
     auto all_nodes_RRT_star = coordination_RRT_star.get_all_nodes();
 
     visualizer.draw_time(AGV_amount, path_RRT_star, all_nodes_RRT_star, all_collisions_time, "rrt_star_time_canvas");
+
+    // Construct the connected RRT* and determine the time path
+    std::cout << "Constructing the connected RRT*..." << std::endl;
+
+    auto coordinated_RRT_star = ConnectedRRTStar(AGV_amount, all_collisions_dimensions, all_collisions_time);
+    coordinated_RRT_star.show_longest_generation();
+    auto path_coordinated_RRT_star = coordinated_RRT_star.get_final_path();
+    auto all_nodes_coordinated_RRT_star = coordinated_RRT_star.get_all_nodes();
+
+    visualizer.draw_time(AGV_amount, path_coordinated_RRT_star, all_nodes_coordinated_RRT_star, all_collisions_time, "connected_rrt_star_time_canvas");
 
     path_finding_app->Run();
     return 0;
